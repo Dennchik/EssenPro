0d6fdaafece6ff1939b6c48e0aa3e7b0734a8692
Alexandr <104072970+Dennchik@users.noreply.github.com>
2024-06-20 14:30:12 +0300

    20 Jun 2024

--- #src/js/main.js
+++ #src/js/main.js
@@ -19,3 +19,4 @@ function addClassOpen() {
 //* ----------------------------------------------------------------------------
 console.log("%c РОССИЯ ", "background: blue; color: yellow; font-size: x-large; border-left: 5px solid black; border-top: 30px solid white; border-right: 2px solid black; border-bottom: 30px solid red;");
 //* ----------------------------------------------------------------------------
+
